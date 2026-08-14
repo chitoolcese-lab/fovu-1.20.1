c679f849e8d86e7eb8cc655337d1059b9fca0294
chitoolcese-lab <chitoolcese@gmail.com>
2026-08-14 14:48:01 -0500

    Update FovUClient.java

--- src/main/java/dev/tori/fovu/client/FovUClient.java
+++ src/main/java/dev/tori/fovu/client/FovUClient.java
@@ -1,1 +1,15 @@
+package dev.tori.fovu.client;
 
+import net.fabricmc.api.ClientModInitializer;
+
+public class FovUClient implements ClientModInitializer {
+
+    public static final String MOD_ID = "fovu";
+    public static final int MIN_FOV = 1;
+    public static final int MAX_FOV = 170;
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
